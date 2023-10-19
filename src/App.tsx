4452ee31f59379b58b4d7200c65839fa5c720972
Apeksh Agarwal <apekshagarwal.742@gmail.com>
2023-10-19 15:44:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// Adding comment
+// Adding comment for slack testing
 function App() {
   return (
     <div className="App">
